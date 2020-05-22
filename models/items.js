117594df9cfaeb7417af6d74750f3eeeb28e62ad
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-05-22 13:18:22 -0400

    installed react and created schema

---
+++ models/items.js
@@ -0,0 +1,12 @@
+const {Schema, model} = require('mongoose');
+
+const itemSchema = new Schema ({
+    name: {type: String, required: true},
+    type: {type: String, required: true},
+    img: String,
+    dateOpened: String,
+    expiresIn: {type: Number, required: true},
+    notes: String
+}, {timestamps: true})
+
+module.exports = model('Item', itemSchema);
--- server.js
+++ server.js
@@ -34,6 +34,8 @@ app.use(express.urlencoded({ extended: false }));// extended: false - does not a
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 //___________________
 // Routes
 //___________________
