54859c4ee5fc5970054c74a9688e5e6c50290ba8
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-05-22 15:28:40 -0400

    added views and styled jumbotron on index page

--- views/Index.jsx
+++ views/Index.jsx
@@ -5,7 +5,13 @@ class Index extends React.Component {
     render() {
         return(
             <Layout>
-                <h1>hello its working</h1>
+                <div className="jumbotron jumbotron-fluid">
+                    <div className="container header-text">
+                        <h1>What's On Your Vanity?</h1>
+                        <p>~ Maintain a healthy skincare routine by organizing and keeping track of your skincare products ~</p>
+                        <a className="btn btn-primary btn-lg" href={`/items/add`} role="button">Add An Item</a>
+                    </div>
+                </div>
             </Layout>
         )
     }
--- views/components/Layout.jsx
+++ views/components/Layout.jsx
@@ -6,6 +6,7 @@ class Layout extends React.Component {
             <html>
                 <head>
                     <title>What's On Your Vanity?</title>
+                    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous"></link>
                     <link rel="stylesheet" href="/css/style.css"/>
                 </head>
                 <body>{this.props.children}</body>
