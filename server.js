758db2b6ceef8e4114822915872a596703e18dd9
Sharon Kim <carolinek@SKs-MacBook-Air-83.lan>
2020-11-09 21:33:09 -0500

    atlas migration

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ const PORT = process.env.PORT || 3000;
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/project2';
+const DB_URI = process.env.DB_URI || 'mongodb://localhost/project2';
 // Connect to Mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true });
+mongoose.connect(DB_URI ,  { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true });
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('connected', () => console.log('mongo connected: ', DB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 // open the connection to mongo
 db.on('open' , ()=>{});
